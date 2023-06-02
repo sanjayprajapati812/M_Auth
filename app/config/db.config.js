5a7efb96cbefa905f1b617225e3a6bd48dc08c86
sanjayprajapati812 <svprajapati812@gmail.com>
2023-06-02 22:53:20 +0530

    live stream

---
+++ app/config/db.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+     url: "mongodb+srv://sanjay:sanjay123@cluster0.ey4kc9v.mongodb.net/test?retryWrites=true&w=majority"
+      };
+
+     // mongodb+srv://<username>:<password>@cluster0.ey4kc9v.mongodb.net/test
---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:4200"
+};
+
+app.use(cors(corsOptions));
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect("mongodb+srv://sanjay:sanjay123@cluster0.ey4kc9v.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my application." });
+});
+
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 4040;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
